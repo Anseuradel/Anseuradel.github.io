95cf6f1a037b887342c73108a8b7c7ad03c87f6b
Anseur Adel <117813369+Anseuradel@users.noreply.github.com>
2025-07-16 21:31:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-const links = document.querySelectorAll('.nav-link');
-const sections = document.querySelectorAll('.section');
-
-links.forEach(link => {
-    link.addEventListener('click', (e) => {
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
         e.preventDefault();
-        const target = link.dataset.section;
-
-        sections.forEach(sec => sec.classList.remove('active'));
-        document.getElementById(target).classList.add('active');
-
-        links.forEach(l => l.classList.remove('active'));
-        link.classList.add('active');
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
